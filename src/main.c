f4600d642d177ea54b69d60275ef332d6a064882
Jimmy Brisson <theotherjimmy@gmail.com>
2015-09-23 21:41:41 -0500

    now receives commands from Yahweh

--- src/main.c
+++ src/main.c
@@ -3,64 +3,65 @@
 #include <RASLib/inc/motor.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include <RASLib/inc/uart.h>
 
+tUART * uart5;
 static int ledState = 0;
 
 void ToggleLED (void) {
         SetPin(PIN_F1, ledState & 1);
-        SetPin(PIN_F2, ledState & 2);
-        SetPin(PIN_F3, ledState & 4);
         ++ledState;
+        while (fKeyWasPressed(uart5)) {
+                Putc(fGetc(uart5));
+        }
 }
-
-struct outputVector {
-        float x, y, w;
-};
-
-struct motorState {
-        struct motorState * nextState;
-        struct outputVector out;
-};
-
-static struct motorState states[3] = {
-          { .nextState = &states[1]
-            , .out = { .x = 0.0, .y = 0.2, .w = 0.0}}
-        , { .nextState = &states[2]
-            , .out = { .x = 0.2, .y = 0.0, .w = 0.0}}
-        , { .nextState = &states[0]
-            , .out = { .x = 0.0, .y = 0.0, .w = 0.2}}
-
-};
-
-static struct motorState *curState = &states[0];
 
 static tMotor * motors[4];
 
-void doMotorState (void) {
-        struct outputVector out = curState->out;
-        SetMotor(motors[0], - out.w - out.y + out.x);
-        SetMotor(motors[1], - out.w - out.y - out.x);
-        SetMotor(motors[2], - out.w + out.y + out.x);
-        SetMotor(motors[3], - out.w + out.y - out.x);
-        curState = curState->nextState;
+void doMotorState (float x, float y, float w) {
+        SetMotor(motors[0], - w - x + y);
+        SetMotor(motors[1], - w - x - y);
+        SetMotor(motors[2], - w + x + y);
+        SetMotor(motors[3], - w + x - y);
 }
 
 int main (void) {
         int i = 0;
-        int pid = 0;
+        uart5 = InitializeUARTModule(5, 115200);
         for (; i < 4; ++i) {
                 motors[i] = InitializeServoMotor(i + PIN_D0, false);
         }
-        pid = CallEvery(ToggleLED, 0, 0.25);
-        doMotorState();
-        Wait(2.0);
-        doMotorState();
-        Wait(2.0);
-        doMotorState();
-        Wait(2.0);
-        CallStop(pid);
-        for(i = 0; i < 4; ++i) SetMotor(motors[i], 0.0);
-        ToggleLED();
-        while (1)
-                asm volatile ("nop");
+        CallEvery(ToggleLED, 0, 0.25);
+        fPrintf(uart5, "$$$");
+        fScanf(uart5, "CMD\n\r");
+        Wait(0.1);
+        Printf("entered command mode\n");
+        fPrintf(uart5, "SM,0\r");
+        fScanf(uart5, "\rAOK\n\r");
+        Wait(0.1);
+        Printf("set master/slave mode\n");
+        fPrintf(uart5, "SN,Moses\r");
+        fScanf(uart5, "\rAOK\n\r");
+        Wait(0.1);
+        Printf("set names\n");
+        fPrintf(uart5, "---\r");
+        fScanf(uart5, "\rEND\n\r");
+        Wait(0.1);
+        Printf("Waiting for connection\n");
+        SetPin(PIN_F2, 1);
+        /* Wait for conection from YahWeh */
+        while (!(fKeyWasPressed(uart5) && fGetc(uart5) == 'Y'));
+        Printf("Connected!");
+        while (1) {
+                float x, y, w;
+                while (!fKeyWasPressed(uart5));
+                if (fGetc(uart5) != 'G') {
+                        for(i = 0; i < 4; ++i) SetMotor(motors[i], 0.0);
+                        continue;
+                }
+                x = ((signed char) fGetc(uart5)) * 0.25f/127.0f;
+                y = ((signed char) fGetc(uart5)) * 0.25f/127.0f;
+                w = ((signed char) fGetc(uart5)) * 0.25f/127.0f;
+                doMotorState(x,y,w);
+        }
 }
